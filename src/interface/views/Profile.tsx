f95d2eb8287ad2872793af6b04764025b8c90656
Luis Valencia <luicciano.08.angel.2002@gmail.com>
2025-03-17 08:13:39 -0400

    fix: update text color in Profile component for better visibility

--- src/interface/views/Profile.tsx
+++ src/interface/views/Profile.tsx
@@ -6,13 +6,13 @@ const Profile = () => {
             <Card className="w-fit min-w-72">
                 <div className="p-4 ">
                     <Avatar img={''} rounded size="xl" color="success" className="mx-auto" />
-                    <div className="text-text-tertiary text-start pt-4">
+                    <div className="text-white text-start pt-4">
                         <p className="text-xl font-medium text-center">John Doe</p>
 
-                        <p className="text-sm text-text-tertiary">Usuario: john</p>
-                        <p className="text-sm text-text-tertiary">Correo: john@example.com</p>
-                        <p className="text-sm text-text-tertiary">Teléfono: +56 987654321</p>
-                        <p className="text-sm text-text-tertiary">Dirección: Sabanita, Boconó</p>
+                        <p className="text-sm text-gray-200">Usuario: john</p>
+                        <p className="text-sm text-gray-200">Correo: john@example.com</p>
+                        <p className="text-sm text-gray-200">Teléfono: +56 987654321</p>
+                        <p className="text-sm text-gray-200">Dirección: Sabanita, Boconó</p>
                     </div>
                 </div>
             </Card>
